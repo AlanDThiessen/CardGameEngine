1c248777477037fdbf1b4f7ac73ef92d03174f9a
David Doughty <doughty@sleepypenguin.net>
2013-03-01 00:49:47 +0000

    List of game types loaded from xml directory

--- src/cge-server.php
+++ src/cge-server.php
@@ -3,6 +3,42 @@
 // functions loaded by main plugin code via add_action & add_shortcode
 
 require('cge-config.php');
+require('cge-database.php');
+
+function cge_show_launch_link() {
+
+	echo '<div id="cge">';
+	echo '<div id="content">';
+	echo '<div id="welcome">';
+	echo 'Welcome to the ' . CGENAME;
+
+	$user = wp_get_current_user();
+	// Stop here if the user isn't logged in.
+	if (!$user->ID) {
+		echo '!  Please <a href="' . site_url() . '/wp-login.php">log in</a> to play.';
+		echo '</div>';
+	} else {
+		// OK, they're logged in, now make sure the database is initialized
+		$dbinit = cge_db_init();
+		if ($dbinit === false) {
+			echo '. <div class="error">Unable to create database.</div>';
+		} else {
+			// now greet user by name and show launch link
+			echo ', ' . $user->display_name . '!';
+			echo '</div>';
+			echo '<input type="hidden" id="sse-server" value="' . plugins_url( '/cge-sse-server.php', __FILE__  ) . '" />';
+			echo '<div id="result">';
+			echo '</div>';
+			echo '<div id="cge_display">';
+			echo '<div id="cge_header"></div>';
+			echo '<div id="cge_window"></div>';
+			echo '<div id="cge_footer"></div>';
+			echo '</div>';
+		}
+	}
+	echo '</div>';
+	echo '</div>';
+}
 
 function cge_enqueue_css() {
 	wp_register_style( 'cge_style', plugins_url( '/css/card-game-engine.css', __FILE__  ), array(  ), '20130130', 'all');
@@ -33,53 +69,20 @@ function cge_get_user() {
 	die();
 }
 
-function cge_show_launch_link() {
-	echo '<div id="cge">';
-	echo '<div id="content">';
-	echo '<div id="welcome">';
-	echo 'Welcome to the ' . CGENAME;
-
-	$user = wp_get_current_user();
-	// Stop here if the user isn't logged in.
-	if (!$user->ID) {
-		echo '!  Please <a href="' . site_url() . '/wp-login.php">log in</a> to play.';
-		echo '</div>';
-	} else {
-		// OK, they're logged in, greet them by name and show launch link
-		echo ', ' . $user->display_name . '!';
-		echo '</div>';
-		echo '<input type="hidden" id="sse-server" value="' . plugins_url( '/cge-sse-server.php', __FILE__  ) . '" />';
-		echo '<div id="result">';
-		echo '</div>';
-		echo '<div id="cge_display">';
-		echo '<div id="cge_header"></div>';
-		echo '<div id="cge_window"></div>';
-		echo '<div id="cge_footer"></div>';
-		echo '</div>';
-	}
-	echo '</div>';
-	echo '</div>';
-}
 function cge_get_game_types() {
-	// @TODO pull these from the db
-	$game_types = array(
-		array(
-			'id' => 'simple-war',
-			'name' => 'Simple War',
-		),
-		array(
-			'id' => 'flex',
-			'name' => 'Flex',
-		),
-		array(
-			'id' => 'rummy',
-			'name' => 'Rummy',
-		),
-		array(
-			'id' => 'ten-phases',
-			'name' => 'Ten Phases',
-		),
-	);
+	$game_types = array();
+	$xml_files = scandir( CGEPATH . 'xml' );
+	foreach ( $xml_files as $xml_file ) {
+		if ( substr( $xml_file, -8, 8 ) == 'game.xml' ) {
+			$game_spec = simplexml_load_file( CGEPATH . 'xml/' . $xml_file );
+			if ( $game_spec[ 'id' ] != null ) {
+				$game_types[] = array(
+					'id'   => (string)$game_spec[ 'id' ],
+					'name' => (string)$game_spec->name
+				);
+			}
+		}
+	}
 
 	echo json_encode($game_types);
 
@@ -111,24 +114,25 @@ function cge_get_joinable_games() {
 	die();
 }
 
-function cge_load_game_spec() {
+function cge_start_game() {
 	$gameTypeId = htmlspecialchars( $_POST[ 'game_type_id' ] );
 	$gameFile = CGEPATH . 'xml/' . $gameTypeId . '-game.xml';
 	if (!file_exists($gameFile)) {
 		die('CGE ERROR: ' . $gameTypeId . ' definition file not found.');
 	} 
-	$gameSpec = simplexml_load_file($gameFile);
-	if ( $gameSpec[ 'id' ] != $gameTypeId ) {
-		die('CGE ERROR: Definition file found is for ' . $gameSpec[ 'id' ] . ', not for ' . $gameTypeId );
+	$game_spec = simplexml_load_file($gameFile);
+	if ( $game_spec[ 'id' ] != $gameTypeId ) {
+		die('CGE ERROR: Definition file found is for ' . $game_spec[ 'id' ] . ', not for ' . $gameTypeId );
 	}
 
-	echo json_encode($gameSpec);
+	// return game spec
+	echo json_encode($game_spec);
 
 	// Need this for Ajax return
 	die();
 }
 
-function cge_load_game_spec_from_id() {
+function cge_join_game() {
 	$gameId = htmlspecialchars( $_POST[ 'game_id' ] );
 	//@TODO: add player to game; sse-server will notify players of change
 	
@@ -142,12 +146,12 @@ function cge_load_game_spec_from_id() {
 	if (!file_exists($gameFile)) {
 		die('CGE ERROR: ' . $gameTypeId . ' definition file not found.');
 	} 
-	$gameSpec = simplexml_load_file($gameFile);
-	if ( $gameSpec[ 'id' ] != $gameTypeId ) {
-		die('CGE ERROR: Definition file found is for ' . $gameSpec[ 'id' ] . ', not for ' . $gameTypeId );
+	$game_spec = simplexml_load_file($gameFile);
+	if ( $game_spec[ 'id' ] != $gameTypeId ) {
+		die('CGE ERROR: Definition file found is for ' . $game_spec[ 'id' ] . ', not for ' . $gameTypeId );
 	}
 
-	echo json_encode($gameSpec);
+	echo json_encode($game_spec);
 
 	// Need this for Ajax return
 	die();
