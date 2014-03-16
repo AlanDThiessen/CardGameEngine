819159a6506279ea23a696109d683197b41664e6
Alan Thiessen <alan@chamrock.net>
2014-03-16 18:46:08 -0400

    Fixed CGEActiveEntity.prototype.AddState() which was coded in haste.

--- src/js/CGEActiveEntity.js
+++ src/js/CGEActiveEntity.js
@@ -26,5 +26,25 @@ CGEActiveEntity.prototype.constructor = CGEActiveEntity;
 
 CGEActiveEntity.prototype.AddState = function( name, parentName )
 {
-   ActiveEntity.call( this, name, parentName );
+   var   parent = undefined;
+
+
+   if( parentName != undefined )
+   {
+      parent = this.topState.FindState( parentName, true );
+   }
+
+   // If we didn't find the state name, or it's undefined, then set the topState
+   // as the parent of the new state.
+   if( parent == undefined )
+   {
+      parent = this.topState;
+   }
+
+   // For now, assume they are valid
+   var state = new CGEState( this, name, parent );
+   parent.AddState( state );
+
+   return( state );
 };
+
