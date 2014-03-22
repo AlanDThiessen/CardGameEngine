59f2c72d711ef18c6f3057cb925624ef3a7ffb41
Jordan Goulder <jordan.goulder@gmail.com>
2014-03-22 14:44:18 -0400

    Add cordova project skeleton

---
+++ src/simple-war-app/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ src/simple-war-app/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes.dex : \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/BuildConfig.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/HelloCordova.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/R$attr.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/R$drawable.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/R$string.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/R$xml.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes/com/example/simplewar/R.class \
+/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/dexedLibs/classes-844e5bb3b29f6bc8da1fc048c93ceb98.jar \
