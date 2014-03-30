f7b4937fb36bcb27b897522242d67708e1eb311c
Alan Thiessen <alan@chamrock.net>
2014-03-29 20:47:41 -0400

    Apparently forgot to add the new CardGameDefs.js to git earlier.

---
+++ src/js/CardGameDefs.js
@@ -0,0 +1,18 @@
+
+
+var CGE_CONSTANTS = {
+
+   /***************************************************************************
+    * SimpleWar Events
+    ***************************************************************************/
+   CGE_EVENT_EXIT             :    0,
+   CGE_EVENT_DO_TRANSACTION   :    1,
+   CGE_EVENT_TRANSACTION      :    2,
+   CGE_EVENT_DEAL             :   10,
+   CGE_EVENT_SCORE            :   11,
+   CGE_EVENT_NOTIFY           :  100,
+   CGE_EVENT_STATUS_UPDATE    :  101
+
+};
+
+module.exports = CGE_CONSTANTS;
