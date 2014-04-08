fe2035c98f17e24cf24e6c820917d44b03e86860
Alan Thiessen <alan@chamrock.net>
2014-04-08 12:33:46 -0400

    Renamed HelloCordova to SimpleWar in Android platform.

--- src/simple-war-app/platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
-/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/HelloCordova.ap_ \
-/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/ant-build/classes.dex \
-/Users/jgoulder/repos/cge/src/simple-war-app/platforms/android/CordovaLib/ant-build/classes.jar \
