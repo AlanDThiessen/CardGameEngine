75e229a4eb831b40b56ff7d0e2aeeefc226ebd9a
Alan Thiessen <alan@chamrock.net>
2014-06-16 22:44:07 -0400

    Added getCGELog.sh utility script.

---
+++ utils/getCGELog.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+##############################################################
+#
+# This script performs a backup of the SimpleWar app data
+# from your phone.  It then converts the backup file to a tar
+# file, and extracts the "cge.log" file from the tar file.
+#
+##############################################################
+#
+# Required:
+# - Android Debug Bridge
+# - Android Backup Extractor:
+#      https://github.com/nelenkov/android-backup-extractor
+#
+##############################################################
+
+adb backup com.example.simplewar
+java -jar abe.jar unpack backup.ab SimpleWar.tar
+tar -xvf SimpleWar.tar --strip-components 4 'apps/com.example.simplewar/f/files/cge.log'
+
