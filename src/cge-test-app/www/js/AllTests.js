cd0acb91064f9afdff13228409a253c310135026
Alan Thiessen <alan@chamrock.net>
2014-08-09 21:38:07 -0400

    Getting common code ready for TestFileModule.js.

--- src/cge-test-app/www/js/AllTests.js
+++ src/cge-test-app/www/js/AllTests.js
@@ -397,11 +397,20 @@ var fileSystem = require("../../../src/js/utils/FileSystem.js");
 describe( "FileModule", function() {
    var fsStatus = false;
    var fsError = null;
+   var callBack = null;    // Each Spec must set this to its respective done callback
 
    beforeEach(function() {
       fsStatus = false;
       fsError = null;
+      callBack = null;
    });
+
+   // This is the global FileSystem error routine for all tests.  When it
+   // encounters an error, it calls the specified callBack routine.
+   var Failure = function(errorCode, errorStr) {
+      fsError = errorStr;
+      Expectations(callBack);
+   };
 
    var Expectations = function(doneCallback) {
       expect(fsError).toBeNull();
@@ -410,21 +419,16 @@ describe( "FileModule", function() {
    };
 
    it("initializes the file system", function(done) {
-
       var OnReady = function(status) {
          fsStatus = status;
          Expectations(done);
       };
 
-      var Failure = function(errorCode, errorStr) {
-         fsError = errorStr;
-         Expectations(done);
-      };
-
+      callBack = done;
       fileSystem.InitFileSystem(OnReady, Failure);
    });
 
-   xit("opens a log file", function() {
+   xit("opens a log file", function(done) {
    });
 
    xit("writes a log file", function() {
