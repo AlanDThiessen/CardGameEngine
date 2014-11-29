26640346eac8f544a9c1dcbcfc446d99c1df52db
Alan Thiessen <alan@chamrock.net>
2014-11-29 15:22:02 -0500

    Refactored CGEClientTest console test application.

--- tests/js/old/CGEClientTest.js
+++ tests/js/old/CGEClientTest.js
@@ -5,78 +5,185 @@ var readline = require('readline');
 
 
 var currMenu   = undefined;
-var currPlayer = undefined;
 var io         = undefined;
 
-var server     = {
-                   hostname:   'gator4021.hostgator.com',
-                   port:       443,
-                   path:       '/~thiessea/TheThiessens.net/cge/cge.php',
-                 };
+var gameTypes = [
+];
+
+var server = {
+    hostname:   'gator4021.hostgator.com',
+    port:       443,
+    path:       '/~thiessea/TheThiessens.net/cge/cge.php',
+};
+
+var cursor = {
+    loc: {
+        x: 0,
+        y: 0
+    }
+};
+
+var users = [
+    {
+        userName: 'testUser1',
+        password: 'user1Pa55w0rd'
+    },
+    {
+        userName: 'testUser2',
+        password: 'user2Pa55w0rd'
+    }
+];
+
+var currUser = users[0];
 
 
-var settingsMenu =   { name: "Settings:",
-                       items: [
-                         { name:    'Return',
-                           enabled: true,
-                           value:   '',
-                           handler: function(){ currMenu = mainMenu },
-                         },
-                         { name:    'Host',
-                           enabled: true,
-                           value:   server.hostname,
-                           handler: SetMenuValue,
-                         },
-                         { name:    'Path',
-                           enabled: true,
-                           value:   server.path,
-                           handler: SetMenuValue,
-                         },
-                         { name:    'Port',
-                           enabled: true,
-                           value:   server.port,
-                           handler: SetMenuValue,
-                         },
-                       ]
-                     };
+var settingsMenu = {
+    name: "Settings:",
+    items: [
+        {
+            name:    'Return',
+            enabled: true,
+            value:   '',
+            handler: function(){ currMenu = mainMenu }
+        },
+        {
+            name:    'Server Settings',
+            enabled: true,
+            value:   '',
+            handler: function(){ currMenu = serverSettingsMenu }
+        },
+        {
+            name:    'User Settings',
+            enabled: true,
+            value:   '',
+            handler: function(){ currMenu = userSettingsMenu }
+        }
+    ]
+};
 
-var mainMenu =       { name: 'Main Menu:',
-                       items: [
-                         { name:    'Exit',
-                           enabled: true,
-                           value:   '',
-                           handler: function(){ process.exit(0) },
-                         },
-                         { name:    'Settings',
-                           enabled: true,
-                           value:   '',
-                           handler: function(){ currMenu = settingsMenu }
-                         },
-                         { name:    'Show Server',
-                           enabled: true,
-                           value:   '',
-                           handler: ShowServer
-                         },
-                         {
-                           name:    'Get Game Types',
-                           enabled: true,
-                           value:   '',
-                           handler: get_game_types
-                         },
-                         {
-                           name:    'Get Deck Speck',
-                           enabled: true,
-                           value:   '',
-                           handler: GetDeckSpec
-                         },
-                         {
-                           name:    'Get User Games',
-                           enabled: true,
-                           value:   '',
-                           handler: GetUserGames
-                         }
-                       ]
-                     };
+var serverSettingsMenu = {
+    name: "Server Settings:",
+    items: [
+        {
+            name: 'Return',
+            enabled: true,
+            value: '',
+            handler: function () {
+                currMenu = settingsMenu
+            }
+        },
+        {
+            name: 'Host',
+            enabled: true,
+            value: server.hostname,
+            handler: SetMenuValue
+        },
+        {
+            name: 'Path',
+            enabled: true,
+            value: server.path,
+            handler: SetMenuValue
+        },
+        {
+            name: 'Port',
+            enabled: true,
+            value: server.port,
+            handler: SetMenuValue
+        }
+    ]
+};
+
+
+var userSettingsMenu = {
+    name: "User Settings:",
+    items: [
+        {
+            name: 'Return',
+            enabled: true,
+            value: '',
+            handler: function () {
+                currMenu = settingsMenu
+            }
+        },
+        {
+            name:    'User1 Username',
+            enabled: true,
+            value:   users[0].userName,
+            handler: SetMenuValue
+        },
+        {
+            name:    'User1 Password',
+            enabled: true,
+            value:   users[0].password,
+            handler: SetMenuValue
+        },
+        {
+            name:    'User2 Username',
+            enabled: true,
+            value:   users[1].userName,
+            handler: SetMenuValue
+        },
+        {
+            name:    'User2 Password',
+            enabled: true,
+            value:   users[1].password,
+            handler: SetMenuValue
+        }
+    ]
+};
+
+
+var mainMenu = {
+    name: 'Main Menu:',
+    items: [
+        {
+            name: 'Exit',
+            enabled: true,
+            value: '',
+            handler: function () {
+                process.exit(0);
+            }
+        },
+        {
+            name: 'Settings',
+            enabled: true,
+            value: '',
+            handler: function () {
+                currMenu = settingsMenu;
+            }
+        },
+        {
+            name: 'Current User: ' + currUser.userName,
+            enabled: true,
+            value: '',
+            handler: ChangeUser
+        },
+        {
+            name: 'Get Game Types',
+            enabled: true,
+            value: '',
+            handler: get_game_types
+        },
+        {
+            name: 'Get Deck Speck',
+            enabled: true,
+            value: '',
+            handler: GetDeckSpec
+        },
+        {
+            name: 'Get User Games',
+            enabled: true,
+            value: '',
+            handler: GetUserGames
+        },
+        {
+            name: 'Get Joinable Games',
+            enabled: true,
+            value: '',
+            handler: GetJoinableGames
+        }
+    ]
+};
                         
 
 function RunMenu() {
@@ -127,84 +234,77 @@ function SetMenuValue(menuItem, value) {
 
 
 function ResetScreen() {
-   //if(io !== undefined) {
-      //readline.cursorTo(io, 0,0);
-      //readline.clearScreenDown(io);
-   //}
    console.log(String.fromCharCode(27) + "[2J" + String.fromCharCode(27) + "[;H" );
 }
 
 
 function DisplayMenu(menu) {
-   //if(io !== undefined) {
-      //io.write('\n' + menu.name);
-      console.log('\n' + menu.name);
-      
-      for(cntr = 0; cntr < menu.items.length; cntr++) {
-         outStr = '   ' + (cntr) + '. ' + menu.items[cntr].name;
-         if(menu.items[cntr].value != '') {
-            if(typeof(menu.items[cntr].value) === 'string') {
-               outStr += ": '" + menu.items[cntr].value + "'";
+    cursor.loc.y = 0;
+
+    console.log('\n' + menu.name);
+
+    for (cntr = 0; cntr < menu.items.length; cntr++) {
+        outStr = '   ' + (cntr) + '. ' + menu.items[cntr].name;
+
+        if (menu.items[cntr].value != '') {
+            if (typeof(menu.items[cntr].value) === 'string') {
+                outStr += ": '" + menu.items[cntr].value + "'";
             }
             else {
-               outStr += ": " + menu.items[cntr].value;
+                outStr += ": " + menu.items[cntr].value;
             }
-         }
-         
-         console.log(outStr);
-      }
-      
-      console.log('');
-   //}
-}
+        }
 
+        console.log(outStr);
+        cursor.loc.y += 1;
+    }
+
+    console.log('');
+    cursor.loc.y += 1;
+}
 
 
 function main() {
    currMenu = mainMenu;
    io = readline.createInterface(process.stdin, process.stdout);
-   SetMenuValue("path", server, 'path', 'alan thiessen');
 
    if(io !== undefined) {
       RunMenu();
       io.on('line', HandleInput);
    }
-
-   //register_user('athiessen', 'hello', "Alan Thiessen", 'AlanDThiessen@gmail.com');
-   //register_user('S', 'hello', "Alan Thiessen", 'AlanDThiessen@gmail.com');
-   //register_user('John', 'hola', "John", '');
-   //login_user('athiessen', 'hello');
-   //get_game_types();
-   //start_game(1, 'simple-war');
-   //get_joinable_games();
-   //get_my_games(1);
 }
 
 
-function ShowServer() {
-   io.question(console.log(server), function(){});
+function ChangeUser() {
 }
 
 
 function GetDeckSpec() {
-   get_deck_spec('asdf');
+    get_deck_spec('asdf');
 }
+
 
 function GetUserGames() {
-   get_my_games('0001');
+    get_my_games('0001');
 }
 
+
+function GetJoinableGames() {
+    get_joinable_games('0002');
+}
+
+
 function server_post(post_data, callback) {
-   var options = {
-                  hostname:   server.hostname,
-                  port:       server.port,
-                  path:       server.path,
-                  method:     'POST',
-                  headers:    {
-                                 'Content-Type': 'application/x-www-form-urlencoded',
-                                 'Content-Length': post_data.length
-                           	}
-                  };
+    var options = {
+        hostname: server.hostname,
+        port: server.port,
+        path: server.path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+        }
+    };
    
    //var post_req = http.request(options, callback);
    var post_req = https.request(options, callback);
@@ -214,13 +314,13 @@ function server_post(post_data, callback) {
    post_req.end();
 }
 
-function log_response(res) {
-   console.log("statusCode: ", res.statusCode);
-   console.log("headers: ", res.headers);
+function LogResponse(res) {
+    console.log("statusCode: ", res.statusCode);
+    console.log("headers: ", res.headers);
 
-   res.on('data', function(d) {
-     process.stdout.write(d);
-   });
+    res.on('data', function (d) {
+        process.stdout.write(d);
+    });
 }
       
 
@@ -233,10 +333,20 @@ function register_user(username, password, displayName, email) {
       'email': email
    });
 
-   server_post(post_data, log_response);
+   server_post(post_data, LogResponse);
    //user = $.parseJSON(response);
 }
-      
+
+
+function SetAjaxValue(resp, value) {
+    if(resp.readyState == resp.DONE) {
+        if(resp.status == 200) {
+            if(typeof success === 'function') {
+                value = JSON.parse(resp.responseText);
+            }
+        }
+    }
+}
 
 function login_user(username, password) {
    var post_data = qs.stringify({
@@ -245,7 +355,7 @@ function login_user(username, password) {
       'password': password,
    });
 
-   server_post(post_data, log_response);
+   server_post(post_data, LogResponse);
    //user = $.parseJSON(response);
 }
       
@@ -256,8 +366,7 @@ function get_my_games(userId) {
       'user_id': userId
    });
 
-   server_post(post_data, log_response);
-   //user = $.parseJSON(response);
+   server_post(post_data, LogResponse);
 }
 
 
@@ -266,8 +375,10 @@ function get_game_types() {
       'action': 'cge_get_game_types'
    });
 
-   server_post(post_data, log_response);
-   //user = $.parseJSON(response);
+   server_post(post_data, function(response){
+       SetAjaxValue(response, gameTypes);
+       LogResponse(response);
+   });
 }
 
 
@@ -277,7 +388,7 @@ function get_deck_spec(name) {
       'deck_type': name
    });
 
-   server_post(post_data, log_response);
+   server_post(post_data, LogResponse);
    //user = $.parseJSON(response);
 }
 
@@ -288,10 +399,10 @@ function get_joinable_games(userId) {
       'user_id': userId
    });
 
-   server_post(post_data, log_response);
+   server_post(post_data, LogResponse);
    //user = $.parseJSON(response);
 }
-      
+
 
 function start_game(userId, gameType) {
    var post_data = qs.stringify({
@@ -300,7 +411,7 @@ function start_game(userId, gameType) {
       'game_type_id': gameType
    });
 
-   server_post(post_data, log_response);
+   server_post(post_data, LogResponse);
    //user = $.parseJSON(response);
 }
 
