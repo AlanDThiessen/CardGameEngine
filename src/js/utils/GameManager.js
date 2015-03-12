6fb93b79eacdc0561e49ccda2cddec9af417f78c
Alan Thiessen <alan@chamrock.net>
2015-03-11 21:46:32 -0400

    Dependency injection now works with example in TestGameManager.js.

--- src/js/utils/GameManager.js
+++ src/js/utils/GameManager.js
@@ -1,6 +1,19 @@
 
+angular.module('myServiceModule', []).
+    factory('notify', ['$window', function(win) {
+        var msgs = [];
+        return function(msg) {
+            msgs.push(msg);
+            if (msgs.length == 3) {
+                win.alert(msgs.join("\n"));
+                msgs = [];
+            }
+        };
+    }]);
+
+/*
 angular.module("cge.utils.gameManager", []).
-    factory('cge.utils.GameManager', ['$window', function(win) {
+    factory('cge.utils.GameManager', [$window, function(win) {
 
     var msgs = [];
 
@@ -13,3 +26,4 @@ angular.module("cge.utils.gameManager", []).
     };
 
 }]);
+*/
--- tests/js/specs/TestGameManager.js
+++ tests/js/specs/TestGameManager.js
@@ -1,11 +1,14 @@
 
-// Pull in the module we're testing.
-//var fileSystem = require("../../../src/js/utils/GameManager.js");
-var gameDataMgr;
-var mock;
+
+angular.module('TestGameManager', []);
 
 
 describe("GameManager", function () {
+    var gameDataMgr;
+    var mock;
+
+    // Before each, make sure the module under test is loaded
+    beforeEach(module('myServiceModule'));
     beforeEach(function() {
         mock = {alert: jasmine.createSpy()};
 
@@ -14,23 +17,46 @@ describe("GameManager", function () {
         });
 
         inject(function($injector) {
-            gameDataMgr = $injector.get('cge.utils.GameManager');
+            notify = $injector.get('notify');
         });
+
+/*
+        angular.module('mock', []).config(['$provide', function($provide) {
+            $provide.factory('winder', function() {
+                return mock;
+            });
+        }]);
+
+        var inject = angular.injector(['cge.utils.gameManager']);
+        gameDataMgr = inject.get('cge.utils.GameManager');
+*/
+
     });
 
     it('should not alert first two notifications', function() {
-        gameDataMgr('one');
-        gameDataMgr('two');
+        notify('one');
+        notify('two');
 
         expect(mock.alert).not.toHaveBeenCalled();
     });
 
     it('should alert all after third notification', function() {
-        gameDataMgr('one');
-        gameDataMgr('two');
-        gameDataMgr('three');
+        notify('one');
+        notify('two');
+        notify('three');
 
         expect(mock.alert).toHaveBeenCalledWith("one\ntwo\nthree");
     });
 
+    it('should clear messages after alert', function() {
+        notify('one');
+        notify('two');
+        notify('third');
+        notify('more');
+        notify('two');
+        notify('third');
+
+        expect(mock.alert.calls.count()).toEqual(2);
+        expect(mock.alert.calls.mostRecent().args).toEqual(["more\ntwo\nthird"]);
+    });
 });
