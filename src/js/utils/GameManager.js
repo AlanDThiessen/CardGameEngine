317dc57869e3d80d7e59208043f1f2ce6fc02042
Alan Thiessen <alan@chamrock.net>
2015-06-27 15:53:46 -0400

    GameManager is now integrated into Angular.

--- src/js/utils/GameManager.js
+++ src/js/utils/GameManager.js
@@ -1,29 +1,4 @@
 
-angular.module('myServiceModule', []).
-    factory('notify', ['$window', function(win) {
-        var msgs = [];
-        return function(msg) {
-            msgs.push(msg);
-            if (msgs.length == 3) {
-                win.alert(msgs.join("\n"));
-                msgs = [];
-            }
-        };
-    }]);
+angular.module("cge.utils").factory('cge.utils.GameManager', function() {
+});
 
-/*
-angular.module("cge.utils.gameManager", []).
-    factory('cge.utils.GameManager', [$window, function(win) {
-
-    var msgs = [];
-
-    return function(msg) {
-        msgs.push(msg);
-        if (msgs.length == 3) {
-            win.alert(msgs.join("\n"));
-            msgs = [];
-        }
-    };
-
-}]);
-*/
--- tests/js/specs/TestGameManager.js
+++ tests/js/specs/TestGameManager.js
@@ -4,59 +4,10 @@ angular.module('TestGameManager', []);
 
 
 describe("GameManager", function () {
-    var gameDataMgr;
-    var mock;
-
-    // Before each, make sure the module under test is loaded
-    beforeEach(module('myServiceModule'));
     beforeEach(function() {
-        mock = {alert: jasmine.createSpy()};
-
-        module(function($provide) {
-            $provide.value('$window', mock);
-        });
-
+        module('cge.utils');
         inject(function($injector) {
-            notify = $injector.get('notify');
+            gameManager = $injector.get('cge.utils.GameManager');
         });
-
-/*
-        angular.module('mock', []).config(['$provide', function($provide) {
-            $provide.factory('winder', function() {
-                return mock;
-            });
-        }]);
-
-        var inject = angular.injector(['cge.utils.gameManager']);
-        gameDataMgr = inject.get('cge.utils.GameManager');
-*/
-
-    });
-
-    it('should not alert first two notifications', function() {
-        notify('one');
-        notify('two');
-
-        expect(mock.alert).not.toHaveBeenCalled();
-    });
-
-    it('should alert all after third notification', function() {
-        notify('one');
-        notify('two');
-        notify('three');
-
-        expect(mock.alert).toHaveBeenCalledWith("one\ntwo\nthree");
-    });
-
-    it('should clear messages after alert', function() {
-        notify('one');
-        notify('two');
-        notify('third');
-        notify('more');
-        notify('two');
-        notify('third');
-
-        expect(mock.alert.calls.count()).toEqual(2);
-        expect(mock.alert.calls.mostRecent().args).toEqual(["more\ntwo\nthird"]);
     });
 });
