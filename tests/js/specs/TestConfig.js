36458b6c9b580d4c0a49c4129a92f3e00d7d7b4a
Alan Thiessen <alan@chamrock.net>
2015-06-29 22:38:49 -0400

    Save off CGE Test App user's configuration before changing it for the test.

--- tests/js/specs/TestConfig.js
+++ tests/js/specs/TestConfig.js
@@ -9,11 +9,35 @@ describe("Config", function() {
     var TEST_PASSWORD  = "TestPassword";
     var TEST_LOG_MASK  = 05;
 
+    var original = {
+        userName: "",
+        password: "",
+        mask: 0,
+        toConsole: false,
+        toFile: false
+    };
+
     beforeEach(function() {
         module('cge.utils');
         inject(function($injector) {
             config = $injector.get('cge.utils.Config');
         });
+
+        // Save off current settings, saved by CGE Test App user
+        original.userName  = config.GetUserName();
+        original.password  = config.GetPassword();
+        original.mask      = config.GetLogMask();
+        original.toConsole = config.GetLogToConsole();
+        original.toFile    = config.GetLogToFile();
+    });
+
+    afterEach(function() {
+        // Restore the users's settings
+        config.SetUserName(original.userName);
+        config.SetPassword(original.password);
+        config.SetLogMask(original.mask);
+        config.SetLogToConsole(original.toConsole);
+        config.SetLogToFile(original.toFile);
     });
 
     it("sets/gets UserName", function() {
