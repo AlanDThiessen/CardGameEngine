4e5580bb52410d2e63032f3502308c5dd8772b11
Alan Thiessen <alan@chamrock.net>
2015-06-29 22:26:02 -0400

    Removed unnecessary files.

--- tests/js/specs/AllTests.js
+++
@@ -1,8 +0,0 @@
-
-require("./TestFileSystem.js");
-require("./TestLogger.js");
-require("./TestServerInterface.js");
-require("./TestAuthenticator.js");
-require("./TestGameDataManager.js");
-require("./TestGameManager.js");
-
